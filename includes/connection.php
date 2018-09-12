3bbdcf2f9d0e29346d720e9b479aaf39263ffb1d
sven <svenvandijk13@gmail.com>
2018-09-12 23:07:23 +0200

    Initial commit
    First Commit, all the folders and some basic stuff.

---
+++ includes/connection.php
@@ -0,0 +1,13 @@
+<?php
+
+try {
+
+    $pdo = new PDO('mysql:host=localhost;dbname=cms', 'root', 'root');
+
+} catch (PDOException $e) {
+
+    exit('Database error.');
+
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once('includes/connection.php');
+
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <div class="container">
+        <a href="index.php" id="logo">CMS</a>
+
+        <ol>
+            <li>
+                <a href="article.php?id=1">Article title</a>
+            </li>
+        </ol>
+    </div>
+</body>
+</html>
